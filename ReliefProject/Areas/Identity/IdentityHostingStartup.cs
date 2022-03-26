376ea92c0234a90f9c9dd0d095ec7b11a8e400cd
James G <jpgrayson21@gmail.com>
2022-03-26 09:09:42 -0600

    Add project files.

---
+++ ReliefProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReliefProject.Areas.Identity.Data;
+using ReliefProject.Data;
+
+[assembly: HostingStartup(typeof(ReliefProject.Areas.Identity.IdentityHostingStartup))]
+namespace ReliefProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ReliefDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ReliefDbContextConnection")));
+
+                services.AddDefaultIdentity<ReliefProjectUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ReliefDbContext>();
+            });
+        }
+    }
+}
---
+++ ReliefProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReliefProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
